89ad34ddebdebb73323f20abecc5d8737a4a3965
jihye <zehye.01@gmail.com>
2020-01-07 20:18:10 +0900

    PostLike 구현

--- app/posts/urls.py
+++ app/posts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import post_list
+from .views import post_list, post_like
 
 app_name = 'posts'
 
 urlpatterns = [
-    path('', post_list, name='post-list')
+    path('', post_list, name='post-list'),
+    path('<int:pk>/like/', post_like, name='post-like'),
 ]
--- app/posts/views.py
+++ app/posts/views.py
@@ -10,3 +10,22 @@ def post_list(request):
     return render(request, 'posts/post-list.html', context)
 
 
+def post_like(request, pk):
+    """
+    pk가 pk인 post에 대한
+    1. postlike객체를 생성한다.
+    2. 만약 해당 객체가 이미 있다면 삭제한다.
+    3. 완료후 posts:post-list로 redirect한다.
+    :param request:
+    :param pk:
+    :return:
+    """
+    post = Post.objects.get(pk=pk)
+    user = request.user
+
+    post_like_qs = PostLike.objects.filter(user=user, post=post)
+    if post_like_qs.exist():
+        post_like_qs.delete()
+    else:
+        PostLike.objects.create(post=post, user=user)
+    return redirect('posts:post-list')
