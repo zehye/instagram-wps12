1c6253e3c1c7a0ff1328ecaa050c6cdc4f517d45
jihye <zehye.01@gmail.com>
2020-01-21 13:04:50 +0900

    naver login 구현

--- app/members/urls.py
+++ app/members/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import login_view, signup_view, logout_view
+from .views import login_view, signup_view, logout_view, naver_login
 
 app_name = 'members'
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
+    path('naver-login', naver_login, name='naver-login'),
 ]
--- app/members/views.py
+++ app/members/views.py
@@ -1,9 +1,11 @@
-from django.contrib.auth import authenticate, login, get_user_model, logout
+import requests
+from django.contrib.auth import login, get_user_model, logout
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+
+# 장고 기본유저나 Custom유저모델 중, 사용중인 User모델을 가져옴
 from .forms import LoginForm, SignupForm
 
-# 장고 기본 유저나 Cuser유저 모델 중, 사용중인
 User = get_user_model()
 
 
@@ -17,36 +19,44 @@ def login_view(request):
 
     POST요청시, 예제를 보고 적절히 로그인 처리한 후, index로 돌아갈 수 있도록 한다
     로그인에 실패하면 다시 로그인페이지로 이동
-    :param request:
-    :return:
     """
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
             form.login(request)
             return redirect('posts:post-list')
-
     else:
         form = LoginForm()
 
+    # Facebook Login을 위한 a태그의 href주소
+    login_base_url = 'https://nid.naver.com/oauth2.0/authorize'
+    login_params = {
+        'response_type': 'code',
+        'client_id': 'lY0EOU3zT3bFrmdDUvoZ',
+        'redirect_uri': 'http://localhost:8000/members/naver-login/',
+        'state': 'RANDOM_STATE',
+    }
+    login_url = '{base}?{params}'.format(
+        base=login_base_url,
+        params='&'.join([f'{key}={value}' for key, value in login_params.items()])
+    )
+
     context = {
         'form': form,
+        'login_url': login_url,
     }
     return render(request, 'members/login.html', context)
-
-
-def logout_view(request):
-    logout(request)
-    return redirect('members:login')
 
 
 def signup_view(request):
     """
     ! config.views.index 삭제
+
     Template: index.html을 복사해서
               /members/signup.html
     URL:  /
     Form: members.forms.SignupForm
+
     생성에 성공하면 로그인 처리 후 (위의 login_view를 참조) posts:post-list로 redirect처리
     :return:
     """
@@ -64,21 +74,67 @@ def signup_view(request):
     }
     return render(request, 'members/signup.html', context)
 
-    # username = request.POST['username']
-    # email = request.POST['email']
-    # password = request.POST['password']
-    # name = request.POST['name']
-    #
-    # if User.objects.filter(username=username).exists():
-    #     return HttpResponse("이미 사용중인 username 입니다.")
-    # if User.objects.filter(email=email).exists():
-    #     return HttpResponse("이미 사용중인 email 입니다.")
-    #
-    # user = User.objects.create_user(
-    #     username=username,
-    #     email=email,
-    #     password=password,
-    #     name=name,
-    # )
-    # login(request, user)
-    # return redirect('posts:post-list')
+
+def logout_view(request):
+    """
+    GET요청으로 처리함
+    요청에 있는 사용자를 logout처리
+    django.contrib.auth.logout함수를 사용한다
+
+    URL: /members/logout/
+    Template: 없음
+
+    :param request:
+    :return:
+    """
+    logout(request)
+    return redirect('members:login')
+
+
+def naver_login(request):
+    # URL: /members/naver-login/
+    # Secret값: API개요의 Client secret
+    code = request.GET.get('code')
+    state = request.GET.get('state')
+    if not code or not state:
+        return HttpResponse('code또는 state가 전달되지 않았습니다')
+
+    token_base_url = 'https://nid.naver.com/oauth2.0/token'
+    token_params = {
+        'grant_type': 'authorization_code',
+        'client_id': 'lY0EOU3zT3bFrmdDUvoZ',
+        'client_secret': 'zeQvsoDH2Q',
+        'code': code,
+        'state': state,
+        'redirectURI': 'https://localhost:8000/members/naver-login/',
+    }
+    token_url = '{base}?{params}'.format(
+        base=token_base_url,
+        params='&'.join([f'{key}={value}' for key, value in token_params.items()])
+    )
+    response = requests.get(token_url)
+    access_token = response.json()['access_token']
+    print(access_token)
+
+    me_url = 'https://openapi.naver.com/v1/nid/me'
+    me_headers = {
+        'Authorization': f'Bearer {access_token}',
+    }
+    me_response = requests.get(me_url, headers=me_headers)
+    me_response_data = me_response.json()
+    print(me_response_data)
+
+    unique_id = me_response_data['response']['id']
+    print(unique_id)
+
+    # n_{unique_id} 의 username을 갖는 새로운 User를 생성
+    #  ex) n_11041464
+    # 생성한 유저를 login시킴
+    # posts:post-list로 이동시킴
+    naver_username = f'n_{unique_id}'
+    if not User.objects.filter(username=naver_username).exists():
+        user = User.objects.create_user(username=naver_username)
+    else:
+        user = User.objects.get(username=naver_username)
+    login(request, user)
+    return redirect('posts:post-list')
